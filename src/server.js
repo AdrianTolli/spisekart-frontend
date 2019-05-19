d2f9aeba26868ad3dda41a0cb9a0e52e9f4aa886
unknown <adrian.tollisen@hotmail.com>
2019-05-19 15:21:53 +0200

    chenged path to favicon in server

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const favicon = require("express-favicon");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
+app.use(favicon(__dirname + "../public/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../build")));

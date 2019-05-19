996963654387cf1bb4148ced1c43245c6515327d
unknown <adrian.tollisen@hotmail.com>
2019-05-19 16:56:33 +0200

    anything

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(path.join(__dirname, "../public/favicon.ico")));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.get("/ping", function(req, res) {
   return res.send("pong");
 });
